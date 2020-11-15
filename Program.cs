e95f2a8b3dccf793aedc29d4b7baf6d65b5ab236
Rafael-P <r4f4el_porto@hotmail.com>
2020-11-14 22:01:30 -0300

    exercício 3 adicionado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercício_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual seu salário atual ?");
+            int salario = int.Parse(Console.ReadLine());
+
+            double aumento = ((double)30/100)*salario;
+            double reajuste = aumento + salario;
+            
+            if(salario < 500){
+                Console.WriteLine("Seu salário será aumentado em "+aumento+" reais");
+                Console.WriteLine("Seu salário atual é de "+reajuste);
+            }else{
+                Console.WriteLine("Você não tem direito a um aumento salárial");
+            }
+
+        }//FIM DO MAIN
+    }
+}
